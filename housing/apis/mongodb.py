bbf7b8e6a5b2ce66f5267ac4e512ded8f6f1d66a
ravi teja n <ravi@quantumgraph.com>
2015-04-25 17:02:05 +0530

    change

--- housing/apis/mongodb.py
+++ housing/apis/mongodb.py
@@ -37,13 +37,8 @@ class MongoBackend(object):
     port = 27017
     user = None
     password = None
-    database_name = 'qga'
-    _logdata_data_collection = 'logdata'
-    _master_data_collection = 'master'
-    _latest_data_collecton = 'latest'
-    _activity_data_collection = 'activity'
-    _notification_data_collection = 'notification'
-    _error_data_collection = 'error'
+    database_name = 'wordfeed'
+    _words_collection = 'words'
     max_pool_size = 100
     options = None
 
@@ -74,32 +69,13 @@ class MongoBackend(object):
             self.user = config.pop('user', self.user)
             self.password = config.pop('password', self.password)
             self.database_name = config.pop('database', self.database_name)
-            self._logdata_data_collection = config.pop(
-                'logdata_collection', self._logdata_data_collection,
+            self._words_collection = config.pop(
+                'words_collection', self._words_collection,
             )
-            self._master_data_collection = config.pop(
-                'master_collection', self._master_data_collection,
-            )
-            self._latest_data_collecton = config.pop(
-                'latest_collection', self._latest_data_collecton,
-            )
-            self._activity_data_collection = config.pop(
-                'activity_collection', self._activity_data_collection,
-            )
-            self._notification_data_collection = config.pop(
-                'notification_collection', self._notification_data_collection,
-            )
-            self._error_data_collection = config.pop(
-                'error_collection', self._error_data_collection,
-            )
-            self.options = dict(config, **config.pop('options', None) or {})
-
             # Set option defaults
             self.options.setdefault('max_pool_size', self.max_pool_size)
             self.options.setdefault('auto_start_request', False)
-        
-
-
+            
         url = kwargs.get('url')
         if url:
             # Specifying backend as an URL
@@ -136,50 +112,11 @@ class MongoBackend(object):
         return self._get_database()
 
     @cached_property
-    def logdata_collection(self):
+    def words_collection(self):
         """Get the metadata task collection."""
-        collection = self.database[self._logdata_data_collection]
+        collection = self.database[self._words_collection]
 
         # Ensure an index on TIME_STAMP is there, if not process the index
         # in the background. Once completed cleanup will be much faster
         collection.ensure_index('TIME_STAMP', background='true')
-        return collection
-    
-    @cached_property
-    def master_collection(self):
-        """Get the metadata task collection."""
-        collection = self.database[self._master_data_collection]
-        collection.ensure_index('USERID', background='true')
-        return collection
-
-    @cached_property
-    def latest_collection(self):
-        """Get the metadata task collection."""
-        collection = self.database[self._latest_data_collecton]
-        collection.ensure_index('USERID', background='true')
-        return collection
-
-    @cached_property
-    def activity_collection(self):
-        collection = self.database[self._activity_data_collection]
-        collection.ensure_index('USERID',background='true')
-        return collection
-
-    @cached_property
-    def notification_collection(self):
-        collection = self.database[self._notification_data_collection]
-        collection.ensure_index('NOTIFICATION_ID',background='true')
-        return collection
-
-    @cached_property
-    def error_collection(self):
-        collection = self.database[self._error_data_collection]
-        collection.ensure_index('TIME_STAMP',background='true')
-        return collection
-
-
-    @cached_property
-    def referrer_collection(self):
-        """Get the metadata task collection."""
-        collection = self.database["referrer"]
         return collection
